834554bc385bac1dfcf20a319c64b92ee0347a39
sandwichzzy <zzy15198@163.com>
2025-08-09 16:45:55 +0800

    wagmi init & game page-fe

---
+++ src/app/api/route.ts
@@ -0,0 +1,18 @@
+// when the game is inited, the player and dealer will be given 2 random cards respectively
+
+//when hit is clicked, get a random card from the deck and add it to the player hand
+//calculate player hand value
+//if player hand value is 21: player wins, black jack
+//if player hand value is over 21: player loses, bust
+//if player hand value is less than 21: player can choose to hit or stand
+
+//when stand is clicked , the dealer will get a random card from the deck and add it to the dealer hand
+//keep doing this until dealer has 17 or more points
+//calculate dealer hand value
+//dealer hand value is 21:player loses, dealer black jack
+//dealer hand value is over 21: player wins, dealer bust
+//dealer hand is less than 21
+//compare with player hand value
+//player > dealer: player wins
+//player < dealer: player loses
+//player = dealer: draw
